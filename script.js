85d48c78bb795fce2680eb4e31aeff2884640e45
ArliumDev <arliumdev@gmail.com>
2025-01-17 12:18:24 +0000

    project restart. must-to-have features defined in dev notebook

--- script.js
+++ script.js
@@ -11,34 +11,8 @@ function gameBoard() {
       board[i].push("");
     }
   }
+  console.log(board);
   return board;
 }
 
-// Retrieve user input
-
-function getUserInput() {
-  return parseInt(prompt('Enter a number between 1 and 9 and make your move: '));
-}
-
-// Convert input to 2d array coordinates
-
-function convertToCoordinates(input) {
-  const row = Math.floor((input - 1) / 3);
-  const col = (input - 1) % 3;
-  console.log([row, col]);
-  return [row, col];
-}
-
-// Appplies player movement
-
-function playerMovement(board, coordinates, player) {
-  const [row, col] = coordinates;
-  if(board[row][col] === "") {
-    board[row][col] = player;
-    console.log(board);
-  }
-}
-
-const board = gameBoard();
-
-playerMovement(board, convertToCoordinates(getUserInput()), 'X');
+gameBoard();
