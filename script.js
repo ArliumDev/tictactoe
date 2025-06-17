423e0e4aaa51a0d8b34df962f19b7f77e8f06d52
ArliumDev <arliumdev@gmail.com>
2025-06-17 19:13:03 +0100

    more halfway features

--- script.js
+++ script.js
@@ -11,11 +11,20 @@ function gameController() {
     currentPlayerIndex = 1 - currentPlayerIndex;
   }
 
-  return { board, players, getCurrentPlayer, switchPlayer };
-}
+  function handleTurn(cell) {
+    const { row, column } = playerMovement(game, cell);
+    if (row === undefined || column === undefined) {
+      return;
+    }
+    if (checkWinner(game, row, column)) {
+      //Show win victory
+    } else {
+      //Resume game
+    }
+  }
 
-const game = gameController();
-console.log(game);
+  return { board, players, getCurrentPlayer, switchPlayer, handleTurn };
+}
 
 function gameBoard() {
   const rows = 3;
@@ -46,3 +55,52 @@ function playerCreator(name, token) {
 
   return { name, token, sumWins, getWins };
 }
+
+function playerMovement(game, cell) {
+  const currentPlayer = game.getCurrentPlayer();
+  const selectedCell = findCell(cell);
+  let lastColumn = 0;
+  let lastRow = 0;
+  if (game.board[selectedCell[0]][selectedCell[1]] !== ' ') {
+    console.log('Occupied cell');
+    return;
+  } else {
+    game.board[selectedCell[0]][selectedCell[1]] = currentPlayer.token;
+    lastColumn = selectedCell[0];
+    lastRow = selectedCell[1];
+    game.switchPlayer();
+  }
+  return { column: lastColumn, row: lastRow };
+}
+
+function findCell(cell) {
+  const cellsMap = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+  };
+  const coords = cellsMap[cell];
+  return coords;
+}
+
+function checkWinner(game, row, column) {}
+
+const game = gameController();
+
+playerMovement(game, 1);
+playerMovement(game, 2);
+playerMovement(game, 3);
+playerMovement(game, 4);
+playerMovement(game, 5);
+playerMovement(game, 6);
+playerMovement(game, 7);
+playerMovement(game, 8);
+playerMovement(game, 9);
+
+console.log(game.board);
